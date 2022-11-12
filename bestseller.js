bb8005f1af8419debebd65fe627646119a5089d2
Sudip Chowdhury <rexalex826@gmail.com>
2022-11-12 22:51:25 +0530

    two more page added

---
+++ bestseller.js
@@ -0,0 +1,53 @@
+
+    let url = "https://636bd1987f47ef51e13b4348.mockapi.io/Products"
+    let bag = []
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            bag = data
+            console.log(data)
+            displaycard(data)
+        })
+
+        function search(){
+        let ser=document.querySelector("input").value
+        let newdata=bag.filter(function(el){
+            return el.title.toLowerCase().includes(ser.toLowerCase())
+        })
+        displaycard(newdata)
+    }
+
+
+    function displaycard(arr) {
+        document.querySelector("#body").innerHTML = ""
+        arr.forEach((element) => {
+            let div = document.createElement("div")
+
+            let Img = document.createElement("img")
+            Img.setAttribute("src", element.avatar)
+            let Title = document.createElement("h3")
+            Title.innerText = element.title
+            let desc = document.createElement("p")
+            desc.innerText = element.Description
+            let cat = document.createElement("p")
+            cat.innerText = element.category
+            let Price = document.createElement("p")
+            Price.innerText = element.price
+            let btn = document.createElement("button")
+            btn.innerText = "ADD TO BAG"
+            btn.addEventListener("click",()=>{
+                addData("cart",element)
+            })
+
+            div.append(Img, Title, desc, cat, Price, btn)
+            document.querySelector('#body').append(div)
+        });
+    }
+    function addData(key,value){
+       let Data=JSON.parse(localStorage.getItem(key))||[]
+       Data.push(value)
+       localStorage.setItem(key,JSON.stringify(Data))
+     }
+
+
+
